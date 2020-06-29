5b96a5e4dbfe34fb960ddf22db48559ea461816a
abdulhadi101 <get2abdul101@yahoo.com>
2020-06-29 10:38:28 +0100

    Header and footer added

---
+++ app/public/wp-content/themes/eduslack-theme/header.php
@@ -0,0 +1,1 @@
+<h1>Greeting from  the Header area</h1>
--- app/public/wp-content/themes/eduslack-theme/single.php
+++ app/public/wp-content/themes/eduslack-theme/single.php
@@ -1,28 +1,15 @@
-
-
 <?php get_header();
 
 	while (have_posts()) {
 		# code...
 		the_post(); ?>
 
-		<h2><a href="<?php the_permalink();  ?>"><?php the_title(); ?></a></h2>
+		<h2><?php the_title(); ?></h2>
 		<p><?php the_content(); ?></p>
-
-		<hr>
+	
 
 
 	<?php } 
-
 	get_footer();
 	?>
-
-	
-	
-
-
- 
-
-
-
-
+	 

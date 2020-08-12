16e738de4bf29055d44e7f1fb412831c9a4ae365
abdulhadi101 <get2abdul101@yahoo.com>
2020-08-12 14:52:56 +0100

    latest commint on 12 August

--- app/public/wp-content/themes/eduslack-theme/functions.php
+++ app/public/wp-content/themes/eduslack-theme/functions.php
@@ -1,10 +1,23 @@
 <?php 
 
 	function eduslack_files(){
+wp_enqueue_script('main-eduslack-script', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+
 		wp_enqueue_style('eduslack_main_styles', get_stylesheet_uri());
+		wp_enqueue_style('custom-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+		wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
 
 	}
-		add_action('wp_enqueue_scripts', 'eduslack_files')
+		add_action('wp_enqueue_scripts', 'eduslack_files');
+		
+function eduslack_features(){
+
+		add_theme_support( 'title-tag' );
+
+}
+
+		add_action('after_setup_theme', 'eduslack_features')
 	
 
 ?>
--- app/public/wp-content/themes/eduslack-theme/page.php
+++ app/public/wp-content/themes/eduslack-theme/page.php
@@ -4,11 +4,88 @@
 		# code...
 		the_post(); ?>
 
-		<h1>This is a page not a post</h1>
+ <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( $file = '/images/ocean.jpg' ) ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"> <?php the_title() ?></h1>
+      <div class="page-banner__intro">
+        <p>Dont forget to work on this later</p>
+      </div>
+    </div>  
+  </div>
 
-		<h2><?php the_title(); ?></h2>
-		<p><?php the_content(); ?></p>
+  <div class="container container--narrow page-section">
 
+  	<?php 
+  		$parentID = wp_get_post_parent_id(get_the_ID());
+
+  			if ($parentID== 0) { ?>
+
+				
+  			<?php }
+
+  			 else { ?>
+
+  				 <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_the_permalink($parentID)  ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $parentID ) ?></a> <span class="metabox__main"><?php echo the_title(); ?></span></p>
+    </div>
+  			<?php }
+
+  	?>
+
+   
+
+    <?php 
+
+    	$testArray = get_pages(array(
+
+    		'child_of' => get_the_ID()
+    	));
+    if($parentID or $testArray) { ?>
+    
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php  echo get_permalink($parentID)?>"><?php echo get_the_title( $parentID) ?></a></h2>
+      <ul class="min-list">
+
+      	<?php  
+
+      		if($parentID){
+
+      			$findChildOf = $parentID;
+
+      		}
+      		else {
+
+      			$findChildOf = get_the_ID();
+      		}
+
+      	wp_list_pages(array(
+      		'title_li' => NULL,
+      		'child_of' => $findChildOf,
+      		'sort_column' => 'menu_order'
+
+      	));
+
+      	?>
+        
+      </ul>
+    </div>
+
+<?php } ?>
+
+
+    <div class="generic-content">
+<?php  the_content(); ?>
+
+   
+    </div>
+
+  </div>
+
+
+
+
+	
 		
 	<?php } 
 	get_footer();
